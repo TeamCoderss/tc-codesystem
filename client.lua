e0ee9b38523638aefc06fc096ebb091e932d913d
Chris0666 <77915141+Chris0666@users.noreply.github.com>
2021-03-10 21:50:14 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,43 @@
+local display = false
+
+RegisterCommand("TC", function()
+    local playerCoords = GetEntityCoords(PlayerPedId())
+    local x = string.sub(playerCoords.x, 0, 8) 
+    local y = string.sub(playerCoords.y, 0, 8)
+    local z = string.sub(playerCoords.z, 0, 6)
+    tcCoords('tc', 'x = ' .. x .. ', y = ' .. y .. ', z = ' .. z)
+    SetDisplay(not display)
+end)
+
+function tcCoords(type, text)
+	SendNUIMessage({
+        type = type,
+		text = text
+	})
+end
+
+function SetDisplay(bool)
+    display = bool
+    SetNuiFocus(bool, bool)
+    SendNUIMessage({
+        type = "ui",
+        status = bool,
+    })
+end
+
+RegisterNUICallback("tccloseButton", function(data)
+    SetNuiFocus(false, false)
+    SetDisplay(false)
+end)
+
+Citizen.CreateThread(function()
+    while display do
+        Citizen.Wait(0)
+        DisableControlAction(0, 1, display)
+        DisableControlAction(0, 2, display)
+        DisableControlAction(0, 142, display)
+        DisableControlAction(0, 18, display)
+        DisableControlAction(0, 322, display)
+        DisableControlAction(0, 106, display)
+    end
+end)
